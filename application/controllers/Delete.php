2fc7d6dcf04f3abf2225cba216a067be5c0518ef
shihastz <shihast0503@gmail.com>
2019-06-01 13:59:30 +0530

    small changes

--- application/controllers/Delete.php
+++ application/controllers/Delete.php
@@ -5,7 +5,8 @@ function __construct(){
 parent::__construct();
 $this->load->model('delete_model');
 }
-// Function to Fetch selected record from database.
+// Function to Fetch selected .
+// record from database.
 function show_student_id() {
 $id = $this->uri->segment(3);
 $data['students'] = $this->delete_model->show_students();
@@ -18,5 +19,5 @@ $id = $this->uri->segment(3);
 $this->delete_model->delete_student_id($id);
 $this->show_student_id();
 }
-}
+} 
 ?>
